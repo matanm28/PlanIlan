bf9d8cbb245426b168055da2fc04d9a0141460f0
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-02-20 23:07:55 +0200

    Changed the sign up page and added login

--- PlanIlanApp/views.py
+++ PlanIlanApp/views.py
@@ -9,11 +9,19 @@ def home(request):
 
 def login(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        u = User.create(user_name=username, email=email, password=password)
-        u.save()
-        return render(request, 'PlanIlanApp/home.html')
+        if 'login' in request.POST.values():
+            username = request.POST.get('username')
+            u = User.get_user_by_user_name(username)
+            if u is not None:
+                context = {'username': username}
+                return render(request, 'PlanIlanApp/home.html', context)
+        else:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            u = User.create(user_name=username, email=email, password=password)
+            u.save()
+            context = {'username': username}
+            return render(request, 'PlanIlanApp/home.html', context)
     else:
         return render(request, 'PlanIlanApp/login.html')
