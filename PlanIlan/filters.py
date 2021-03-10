e3f85f6f5de924f31c84f124764fe54341180a7b
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-03-10 20:33:39 +0200

    Added initial search engine

--- PlanIlan/filters.py
+++ PlanIlan/filters.py
@@ -1,8 +1,15 @@
 import django_filters
+from django_filters.widgets import BooleanWidget
+
 from .models import *
+from django_filters import *
+from django import forms
 
 
 class CourseFilter(django_filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+    locations__online = BooleanFilter(widget=BooleanWidget())
+
     class Meta:
         model = Course
-        fields = ['name', 'code', 'rating', 'teachers', 'session_times', 'locations']
+        fields = ['name', 'rating', 'teachers']
