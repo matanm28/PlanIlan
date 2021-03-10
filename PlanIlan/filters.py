54bcb1218eabcd288b7f3bb26611e687cda64d76
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-03-10 22:25:01 +0200

    Use django-widgets for more flexebility

--- PlanIlan/filters.py
+++ PlanIlan/filters.py
@@ -1,15 +1,22 @@
 import django_filters
+from django_filters import *
 from django_filters.widgets import BooleanWidget
 
 from .models import *
-from django_filters import *
-from django import forms
 
 
 class CourseFilter(django_filters.FilterSet):
     name = CharFilter(field_name='name', lookup_expr='icontains')
     locations__online = BooleanFilter(widget=BooleanWidget())
+    rating = ChoiceFilter(choices=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')),
+                          field_name='rating__average')
+    start_time = TimeFilter(field_name='session_times__start_time', lookup_expr='gt')
+    end_time = TimeFilter(field_name='session_times__end_time', lookup_expr='lt')
+    TEACHER_CHOICES = [
+        (i, t) for i, t in enumerate(Teacher.objects.all())
+    ]
+    teachers = ChoiceFilter(field_name='teachers', choices=TEACHER_CHOICES)
 
     class Meta:
         model = Course
-        fields = ['name', 'rating', 'teachers']
+        fields = ['teachers']
