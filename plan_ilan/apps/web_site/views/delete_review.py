2da73238fbefe94c081a822d038774a504115225
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-06-01 13:08:58 +0300

    Working on rating deletion

--- plan_ilan/apps/web_site/views/delete_review.py
+++ plan_ilan/apps/web_site/views/delete_review.py
@@ -1,15 +1,24 @@
 from django.urls import reverse_lazy
 from django.views import generic
 
-from plan_ilan.apps.web_site.models import Review
+from plan_ilan.apps.web_site.models import Review, TeacherReview, TeacherRating, Rating, CourseRating, CourseReview
 
 
 class ReviewDeleteView(generic.DeleteView):
     model = Review
 
-    def get_success_url(self):
-        self.object = self.get_object()
-        return reverse_lazy('teacher_detail', kwargs={'pk': self.object.pk}, )
-
     def get(self, *args, **kwargs):
         return self.post(*args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if isinstance(self.object, TeacherReview):
+            rating = TeacherRating.objects.filter(user=request.user, teacher=self.object.teacher)
+            id_to_transfer = self.object.teacher.pk
+        else:
+            rating = CourseRating.objects.filter(user=request.user, course=self.object.course)
+            id_to_transfer = self.object.course.code
+        self.object.delete()
+        rating.delete()
+        return reverse_lazy('teacher_detail', kwargs={'pk': id_to_transfer}, )
+
