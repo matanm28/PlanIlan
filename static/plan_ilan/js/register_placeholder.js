a0168a5a6edd11c66fe9b090b0345ae3f391b0b1
sapirg50 <sapirg50@users.noreply.github.com>
2021-06-02 21:48:27 +0300

    navbar style  in progress

--- static/plan_ilan/js/register_placeholder.js
+++ static/plan_ilan/js/register_placeholder.js
@@ -1,7 +1,7 @@
 //Query All input fields
 var form_fields = document.getElementsByTagName('input')
 form_fields[1].placeholder = 'שם משתמש...';
-form_fields[2].placeholder = 'אי-מייל...';
+form_fields[2].placeholder = 'אימייל...';
 form_fields[3].placeholder = 'הזן סיסמא...';
 form_fields[4].placeholder = 'הזן סיסמא מחדש...';
 form_fields[5].placeholder = 'שם פרטי...';
