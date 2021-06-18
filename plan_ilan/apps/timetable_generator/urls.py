824382145fc6d45399bea56fe7131d6ccb9a629e
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-06-18 15:50:47 +0300

    Initial commit for timetable view and backend

--- plan_ilan/apps/timetable_generator/urls.py
+++ plan_ilan/apps/timetable_generator/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from plan_ilan.apps.timetable_generator import views
 
 urlpatterns = [
-    path('timetable/', views.time_table, name='timetable'),
+    path('timetable/', views.first_form, name='first-form'),
+    path('timetable/deps', views.pick_departments, name='pick-deps'),
+    path('timetable/courses', views.pick_courses, name='pick-courses'),
+    path('timetable/lessons', views.pick_lessons, name="pick-lessons"),
+    path('timetable/build', views.build_timetable, name="build-timetable")
 ]
--- plan_ilan/apps/timetable_generator/views.py
+++ plan_ilan/apps/timetable_generator/views.py
@@ -1,32 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from plan_ilan.apps.web_site.decorators import authenticated_user
-from plan_ilan.apps.web_site.models import Lesson, Course, Teacher, LessonTime
-from django.core import serializers
-from plan_ilan.apps.web_site.filters import CourseInstanceFilter, TeacherInstanceFilter
-from django.http import JsonResponse
+from plan_ilan.apps.web_site.models import Lesson, Course, Account, Department, Semester
+from .models import RankedLesson, Timetable
 
 
 @authenticated_user
-def time_table(request):
-    if request.method == 'GET':
-        lessons_list = Lesson.objects.all()
-        lesson_filter = CourseInstanceFilter(request.GET, queryset=lessons_list)
-        lessons_list = lesson_filter.qs
-        context = {'lesson_filter': lesson_filter}
-        if request.is_ajax():
-            lessons_pk = list(map(lambda lesson: lesson.pk, lessons_list))
-            course_list = Course.objects.filter(lessons__pk__in=lessons_pk).distinct()
-            teacher_list = Teacher.objects.filter(lessons__pk__in=lessons_pk).distinct()
-            session_list = LessonTime.objects.filter(lessons__pk__in=lessons_pk)
-            json_session_list = serializers.serialize("json", session_list)
-            json_teacher_list = serializers.serialize("json", teacher_list)
-            json_course_list = serializers.serialize("json", course_list)
-            json_lesson_list = serializers.serialize("json", lessons_list)
-            context = {'json_lesson_list': json_lesson_list,
-                       'json_course_list': json_course_list,
-                       'json_teacher_list': json_teacher_list,
-                       'json_session_list': json_session_list}
-            return JsonResponse(context, safe=False)
-        return render(request, 'timetable_generator/timetable.html', context)
-    return render(request, 'timetable_generator/timetable.html')
+def first_form(request):
+    context = {'semesters': Semester.objects.all()}
+    return render(request, 'timetable_generator/first_form.html', context)
+
+
+@authenticated_user
+def pick_departments(request):
+    semester = get_object_or_404(Semester, label=request.GET.get('semester'))
+    table_name = request.GET.get('table-name')
+    learn_days = int(request.GET.get('week-days'))
+    user = get_object_or_404(Account, user=request.user)
+    timetable_user = Timetable.temporal_create(account=user, name=table_name, semester=semester,
+                                               max_num_of_days=learn_days)
+    timetable_user.save()
+    context = {"departments": Department.choices()}
+    return render(request, 'timetable_generator/pick_deps.html', context)
+
+
+@authenticated_user
+def pick_courses(request):
+    timetable_user = Timetable.objects.filter(common_info__account=Account.objects.get(user=request.user)).first()
+    course_elective = Course.objects.filter(department__in=request.POST.getlist("mandatory-deps"),
+                                            lessons__session_times__semester=timetable_user.semester)
+    course_mandatory = Course.objects.filter(department__in=request.POST.getlist("elective-deps"),
+                                             lessons__session_times__semester=timetable_user.semester)
+    context = {"course_elective": course_elective, "course_mandatory": course_mandatory}
+    return render(request, 'timetable_generator/pick_courses.html', context)
+
+
+@authenticated_user
+def pick_lessons(request):
+    mand_courses = request.POST.getlist("mand")
+    mand_list = Course.objects.filter(pk__in=mand_courses)
+    elect_courses = request.POST.getlist("elect")
+    elect_list = Course.objects.filter(pk__in=elect_courses)
+    context = {"mand_list": mand_list, "elect_list": elect_list}
+    return render(request, 'timetable_generator/pick_lessons.html', context)
+
+
+@authenticated_user
+def build_timetable(request):
+    selected_mand_lessons = request.POST.getlist("mand-lessons")
+    ranks_mand = request.POST.getlist("rank-mand-lesson")
+    mand_lessons = Lesson.objects.select_related("course").filter(pk__in=selected_mand_lessons)
+    selected_elect_lessons = request.POST.getlist("elect-lessons")
+    ranks_elect = request.POST.getlist("rank-elect-lesson")
+    elect_lessons = Lesson.objects.select_related("course").filter(pk__in=selected_elect_lessons)
+    ranked_mand = [RankedLesson.create(lesson, rank) for (lesson, rank) in zip(mand_lessons, ranks_mand)]
+    ranked_elect = [RankedLesson.create(lesson, rank) for (lesson, rank) in zip(elect_lessons, ranks_elect)]
+    return render(request, 'timetable_generator/build_timetable.html')
