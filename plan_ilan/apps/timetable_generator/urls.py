2f9341050d4c49890d75b665bd675cfdaa2e846d
mamalka <mamalka@cisco.com>
2021-06-19 11:30:47 +0300

    changes some views in timetable_generator app to class based views adds forms.py with form for timetable generation

--- plan_ilan/apps/timetable_generator/urls.py
+++ plan_ilan/apps/timetable_generator/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, register_converter
+from django.urls import path, register_converter, re_path
 
 from plan_ilan.apps.timetable_generator import views, converters
 
 register_converter(converters.CommaSeparatedIntegerListConverter, 'int_list')
 
 urlpatterns = [
-    path(r'^timetable/', views.FirstView.as_view(), name='first-form'),
-    path(r'^timetable/deps', views.PickDepartmentsView.as_view(), name='pick-deps'),
-    path(r'^timetable/courses$', views.pick_courses, name='pick-courses'),
-    path(r'^timetable/lessons', views.pick_lessons, name="pick-lessons"),
-    path(r'^timetable/build', views.build_timetable, name="build-timetable")
+    path('timetable/', views.FirstView.as_view(), name='first-form'),
+    path('timetable/deps', views.PickDepartmentsView.as_view(), name='pick-deps'),
+    re_path(r'^timetable/courses$', views.pick_courses, name='pick-courses'),
+    path('timetable/lessons', views.pick_lessons, name="pick-lessons"),
+    path('timetable/build', views.build_timetable, name="build-timetable")
 ]
