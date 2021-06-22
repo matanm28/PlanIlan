c5213564f575690b38fcbf9f93d4eea5c8b4ebc4
Yana Molodetsky <yana.molodetsky@live.biu.ac.il>
2021-06-22 19:16:35 +0300

    Finished with the forms and templates, added to admin the models

--- plan_ilan/apps/timetable_generator/urls.py
+++ plan_ilan/apps/timetable_generator/urls.py
@@ -5,7 +5,7 @@ from plan_ilan.apps.timetable_generator import views
 urlpatterns = [
     path('timetable/', views.FirstView.as_view(), name='first-form'),
     path('timetable/deps', views.PickDepartmentsView.as_view(), name='pick-deps'),
-    path('timetable/courses', views.pick_courses, name='pick-courses'),
-    path('timetable/lessons', views.pick_lessons, name="pick-lessons"),
-    path('timetable/build', views.build_timetable, name="build-timetable")
+    path('timetable/courses', views.PickCoursesView.as_view(), name='pick-courses'),
+    path('timetable/lessons', views.PickLessonsView.as_view(), name='pick-lessons'),
+    path('timetable/build', views.BuildTimeTableView.as_view(), name='build-timetable')
 ]
