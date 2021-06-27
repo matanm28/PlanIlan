3d58be97306f39ecf2a9b33a908f8aa1985009c7
mamalka <mamalka@cisco.com>
2021-06-27 11:22:26 +0300

    fixes bug with faculties manager

--- plan_ilan/apps/web_site/managers.py
+++ plan_ilan/apps/web_site/managers.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class CounterManger(models.Manager):
-    def __init__(self, counted_field: str):
-        self.counted_field = counted_field
+    def __init__(self, counted_field: str = None):
         super().__init__()
+        self.counted_field = counted_field
 
     @property
     def with_count(self):
+        assert self.counted_field is not None, "To access 'with_count' property a counted field must be defined " \
+                                               "when initializing this Manger object"
         count_field_name = f'{self.counted_field}_count'
         return self.annotate(**{count_field_name: models.Count(self.counted_field)})
