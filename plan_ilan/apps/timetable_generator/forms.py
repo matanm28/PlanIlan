a67b0bf460ff0572f36385c9c43216a8f8eccfb9
sapirg50 <sapirg50@users.noreply.github.com>
2021-06-27 15:18:46 +0300

    timetable build first step page design - finished

--- plan_ilan/apps/timetable_generator/forms.py
+++ plan_ilan/apps/timetable_generator/forms.py
@@ -6,11 +6,15 @@ from plan_ilan.apps.web_site.models import Semester, Department
 
 class FirstForm(forms.Form):
     name = forms.CharField(max_length=255, label='שם מערכת השעות',
-                           error_messages={'required': 'יא מזדיין שים שם תקין יא מניאק'})
-    semester = forms.ModelChoiceField(Semester.objects.all(), label='סמסטר',
+                           error_messages={'required': 'נא הכנס שם תקין'},
+                           widget=forms.TextInput(attrs={'placeholder': 'שם מערכת שעות...'}))
+    semester = forms.ModelChoiceField(Semester.objects.all(), label='סמסטר', empty_label="סמסטר...",
                                       error_messages={'required': 'נא לבחור סמסטר'})
     max_num_of_days = forms.IntegerField(min_value=1, max_value=6, label='מספר ימי לימוד',
-                                         error_messages={'required': 'יש לבחור כמות ימים בין 1 ל-6'})
+                                         error_messages={'required': 'יש לבחור כמות ימים בין 1 ל-6'},
+                                         widget=forms.NumberInput(
+                                             attrs={'placeholder': "...מס' ימי לימוד",
+                                                    'style': 'width:14ch; text-align: center'}))
 
 
 class DepartmentsForm(forms.Form):
