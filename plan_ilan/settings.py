42e89d335fa19b68ffc8a84d4c6172c723df97c4
mamalka <mamalka@cisco.com>
2021-07-27 21:51:43 +0300

    added ebextensions

--- plan_ilan/settings.py
+++ plan_ilan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '0p!a&2d!ui+clgkdpgj7umd+0$k)m+#@dqacj7)=r)tqyy@_#t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['plan-ilan-env-final.eba-z2efbn7p.eu-central-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['plan-ilan-env-newer.eba-aq8y523e.eu-central-1.elasticbeanstalk.com','127.0.0.1']
 
 # Application definition
 
